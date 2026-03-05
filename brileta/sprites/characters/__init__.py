25e6cd51066548712728b4baca9932265548eb4a
Mark Ayzenshtat <mayz@users.noreply.github.com>
2026-03-05 14:30:22 -0800

    Trim characters __init__.py: remove 19 unexported internal symbols from public API.

--- brileta/sprites/characters/__init__.py
+++ brileta/sprites/characters/__init__.py
@@ -7,8 +7,6 @@ four directional standing poses (front/back/left/right).
 
 from __future__ import annotations
 
-from brileta.types import Facing
-
 from .appearance import (
     CHARACTER_DIRECTIONAL_POSE_COUNT,
     CLOTHING_PALETTES,
@@ -16,35 +14,19 @@ from .appearance import (
     HUMANOID_GLYPHS,
     PANTS_PALETTES,
     POSE_BACK_STAND,
-    POSE_FRONT_STAND,
     POSE_LEFT_STAND,
     POSE_RIGHT_STAND,
     POSE_STAND,
     POSES,
     SKIN_PALETTES,
-    BodyParams,
     CharacterAppearance,
-    Pose,
     character_sprite_seed,
 )
-from .clothing import CLOTHING_DEFS, CLOTHING_STYLE_NAMES
-from .hair import (
-    HAIR_IDX_BALD,
-    HAIR_IDX_LONG,
-    HAIR_IDX_MEDIUM,
-    HAIR_IDX_SHORT,
-    HAIR_IDX_TALL,
-    HAIR_STYLE_NAMES,
-)
+from .clothing import CLOTHING_STYLE_NAMES
+from .hair import HAIR_STYLE_NAMES
 from .renderer import (
     CARICATURE_TEMPLATES,
     draw_character_pose,
-    generate_caricature_belly,
-    generate_caricature_broad,
-    generate_caricature_child,
-    generate_caricature_standard,
-    generate_caricature_thin,
-    generate_character,
     generate_character_pose_set,
     generate_character_sprite,
     roll_character_appearance,
@@ -53,37 +35,21 @@ from .renderer import (
 __all__ = [
     "CARICATURE_TEMPLATES",
     "CHARACTER_DIRECTIONAL_POSE_COUNT",
-    "CLOTHING_DEFS",
     "CLOTHING_PALETTES",
     "CLOTHING_STYLE_NAMES",
-    "HAIR_IDX_BALD",
-    "HAIR_IDX_LONG",
-    "HAIR_IDX_MEDIUM",
-    "HAIR_IDX_SHORT",
-    "HAIR_IDX_TALL",
     "HAIR_PALETTES",
     "HAIR_STYLE_NAMES",
     "HUMANOID_GLYPHS",
     "PANTS_PALETTES",
     "POSES",
     "POSE_BACK_STAND",
-    "POSE_FRONT_STAND",
     "POSE_LEFT_STAND",
     "POSE_RIGHT_STAND",
     "POSE_STAND",
     "SKIN_PALETTES",
-    "BodyParams",
     "CharacterAppearance",
-    "Facing",
-    "Pose",
     "character_sprite_seed",
     "draw_character_pose",
-    "generate_caricature_belly",
-    "generate_caricature_broad",
-    "generate_caricature_child",
-    "generate_caricature_standard",
-    "generate_caricature_thin",
-    "generate_character",
     "generate_character_pose_set",
     "generate_character_sprite",
     "roll_character_appearance",
